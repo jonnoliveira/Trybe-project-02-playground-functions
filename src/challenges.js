86403d73e509a65e4bbe9a0ce774823ce33527a0
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-24 19:14:43 -0300

    Requisito 6 - Completo

--- src/challenges.js
+++ src/challenges.js
@@ -6,9 +6,7 @@ function compareTrue(value1, value2) {
   if (cafe && leite === true) {
     return true;
   }
-
   return false;
-
 }
 
 // Desafio 2
@@ -19,7 +17,7 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(str) {
- return str.split(" ");
+  return str.split(" ");
 
 }
 
@@ -36,43 +34,57 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+
+  let maior = numbers[0];
+  let sum = 0;
+
+  for (let i = 1; i < numbers.length; i += 1) {
+    if (maior < numbers[i]) {
+      maior = numbers[i];
+    }
+  }
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (maior === numbers[i]) {
+      sum += 1;
+    }
+  }
+  return sum;
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+  // Desafio 7
+  function catAndMouse() {
+  }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+  // Desafio 8
+  function fizzBuzz() {
+    // seu código aqui
+  }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+  // Desafio 9
+  function encode() {
+    // seu código aqui
+  }
+  function decode() {
+    // seu código aqui
+  }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+  // Desafio 10
+  function techList() {
+    // seu código aqui
+  }
 
-module.exports = {
-  calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-  techList,
-};
+  module.exports = {
+    calcArea,
+    catAndMouse,
+    compareTrue,
+    concatName,
+    decode,
+    encode,
+    fizzBuzz,
+    footballPoints,
+    highestCount,
+    splitSentence,
+    techList,
+  };
