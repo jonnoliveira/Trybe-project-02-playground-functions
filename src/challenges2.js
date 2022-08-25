bf51d98383f32c56b6a6c8caffbf0f3315a1dd37
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-08-25 00:09:11 -0300

    Requisito 12 - Completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -4,17 +4,28 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+
+  if (lineA > lineB + lineC || lineA < Math.abs(lineB - lineC)) {
+    return false;
+  }
+  else if (lineB > lineA + lineC  || lineB < Math.abs(lineA - lineC)) {
+    return false;
+  }
+  else if (lineC > lineA + lineB || lineC < Math.abs(lineA - lineB)) {
+    return false;
+  }
+  return true;
 }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
 
-module.exports = {
-  generatePhoneNumber,
-  hydrate,
-  triangleCheck,
-};
+  // Desafio 13
+  function hydrate() {
+    // seu código aqui
+  }
+
+  module.exports = {
+    generatePhoneNumber,
+    hydrate,
+    triangleCheck,
+  };
